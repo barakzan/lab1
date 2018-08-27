aea40134ca37e075639da45efb1285bdf1c21bec
mpc3 <mpc3@ef.technion.ac.il>
2018-08-27 19:05:52 +0300

    project: add initial VGA support

--- project/quartus/pins.tcl
+++ project/quartus/pins.tcl
@@ -157,35 +157,35 @@ set_location_assignment PIN_AD20 -to  d_bitrec_up[6]
 ##============================================================
 ## VGA
 ##============================================================
-#set_location_assignment PIN_AK22 -to VGA_BLANK_N
-#set_location_assignment PIN_AJ21 -to VGA_B[0]
-#set_location_assignment PIN_AJ20 -to VGA_B[1]
-#set_location_assignment PIN_AH20 -to VGA_B[2]
-#set_location_assignment PIN_AJ19 -to VGA_B[3]
-#set_location_assignment PIN_AH19 -to VGA_B[4]
-#set_location_assignment PIN_AJ17 -to VGA_B[5]
-#set_location_assignment PIN_AJ16 -to VGA_B[6]
-#set_location_assignment PIN_AK16 -to VGA_B[7]
-#set_location_assignment PIN_AK21 -to VGA_CLK
-#set_location_assignment PIN_AK26 -to VGA_G[0]
-#set_location_assignment PIN_AJ25 -to VGA_G[1]
-#set_location_assignment PIN_AH25 -to VGA_G[2]
-#set_location_assignment PIN_AK24 -to VGA_G[3]
-#set_location_assignment PIN_AJ24 -to VGA_G[4]
-#set_location_assignment PIN_AH24 -to VGA_G[5]
-#set_location_assignment PIN_AK23 -to VGA_G[6]
-#set_location_assignment PIN_AH23 -to VGA_G[7]
-#set_location_assignment PIN_AK19 -to VGA_HS
-#set_location_assignment PIN_AK29 -to VGA_R[0]
-#set_location_assignment PIN_AK28 -to VGA_R[1]
-#set_location_assignment PIN_AK27 -to VGA_R[2]
-#set_location_assignment PIN_AJ27 -to VGA_R[3]
-#set_location_assignment PIN_AH27 -to VGA_R[4]
-#set_location_assignment PIN_AF26 -to VGA_R[5]
-#set_location_assignment PIN_AG26 -to VGA_R[6]
-#set_location_assignment PIN_AJ26 -to VGA_R[7]
-#set_location_assignment PIN_AJ22 -to VGA_SYNC_N
-#set_location_assignment PIN_AK18 -to VGA_VS
+set_location_assignment PIN_AK22 -to VGA_BLANK_N
+set_location_assignment PIN_AJ21 -to VGA_B[0]
+set_location_assignment PIN_AJ20 -to VGA_B[1]
+set_location_assignment PIN_AH20 -to VGA_B[2]
+set_location_assignment PIN_AJ19 -to VGA_B[3]
+set_location_assignment PIN_AH19 -to VGA_B[4]
+set_location_assignment PIN_AJ17 -to VGA_B[5]
+set_location_assignment PIN_AJ16 -to VGA_B[6]
+set_location_assignment PIN_AK16 -to VGA_B[7]
+set_location_assignment PIN_AK21 -to VGA_CLK
+set_location_assignment PIN_AK26 -to VGA_G[0]
+set_location_assignment PIN_AJ25 -to VGA_G[1]
+set_location_assignment PIN_AH25 -to VGA_G[2]
+set_location_assignment PIN_AK24 -to VGA_G[3]
+set_location_assignment PIN_AJ24 -to VGA_G[4]
+set_location_assignment PIN_AH24 -to VGA_G[5]
+set_location_assignment PIN_AK23 -to VGA_G[6]
+set_location_assignment PIN_AH23 -to VGA_G[7]
+set_location_assignment PIN_AK19 -to VGA_HS
+set_location_assignment PIN_AK29 -to VGA_R[0]
+set_location_assignment PIN_AK28 -to VGA_R[1]
+set_location_assignment PIN_AK27 -to VGA_R[2]
+set_location_assignment PIN_AJ27 -to VGA_R[3]
+set_location_assignment PIN_AH27 -to VGA_R[4]
+set_location_assignment PIN_AF26 -to VGA_R[5]
+set_location_assignment PIN_AG26 -to VGA_R[6]
+set_location_assignment PIN_AJ26 -to VGA_R[7]
+set_location_assignment PIN_AJ22 -to VGA_SYNC_N
+set_location_assignment PIN_AK18 -to VGA_VS
 #
 ##============================================================
 ## Audio
