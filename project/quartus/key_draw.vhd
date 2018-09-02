482e590ee70be7241ce7acc136a0bc58c667cdc9
boaz <boaztait@gmail.com>
2018-09-02 22:31:59 +0300

    project: change the graphics from 3 octaves to 2 octaves

--- project/quartus/key_draw.vhd
+++ project/quartus/key_draw.vhd
@@ -22,81 +22,61 @@ end key_draw;
 architecture key_draw_arch of key_draw is 
 	constant SILENCE : std_logic_vector(0 to 6) := "0000000";
 	
-	constant start_note1 : integer := 6;
-	constant end_note1 : integer := 27;
-	constant start_note2 : integer := end_note1 + 3;
-	constant end_note2 : integer := 27 + start_note2;
-	constant start_note3 : integer := end_note2 + 3;
-	constant end_note3 : integer := 27 + start_note3;
-	constant start_note4 : integer := end_note3 + 3;
-	constant end_note4 : integer := 27 + start_note4;
-	constant start_note5 : integer := end_note4 + 3;
-	constant end_note5 : integer := 27 + start_note5;
-	constant start_note6 : integer := end_note5 + 3;
-	constant end_note6 : integer := 27 + start_note6;
-	constant start_note7 : integer := end_note6 + 3;
-	constant end_note7 : integer := 27 + start_note7;
-	constant start_note8 : integer := end_note7 + 3;
-	constant end_note8 : integer := 27 + start_note8;
-	constant start_note9 : integer := end_note8 + 3;
-	constant end_note9 : integer := 27 + start_note9;
-	constant start_note10 : integer := end_note9 + 3;
-	constant end_note10 : integer := 27 + start_note10;
-	constant start_note11 : integer := end_note10 + 3;
-	constant end_note11 : integer := 27 + start_note11;
-	constant start_note12 : integer := end_note11 + 3;
-	constant end_note12 : integer := 27 + start_note12;
-	constant start_note13 : integer := end_note12 + 3;
-	constant end_note13 : integer := 27 + start_note13;
-	constant start_note14 : integer := end_note13 + 3;
-	constant end_note14 : integer := 27 + start_note14;
-	constant start_note15 : integer := end_note14 + 3;
-	constant end_note15 : integer := 27 + start_note15;
-	constant start_note16 : integer := end_note15 + 3;
-	constant end_note16 : integer := 27 + start_note16;
-	constant start_note17 : integer := end_note16 + 3;
-	constant end_note17 : integer := 27 + start_note17;
-	constant start_note18 : integer := end_note17 + 3;
-	constant end_note18 : integer := 27 + start_note18;
-	constant start_note19 : integer := end_note18 + 3;
-	constant end_note19 : integer := 27 + start_note19;
-	constant start_note20 : integer := end_note19 + 3;
-	constant end_note20 : integer := 27 + start_note20;
-	constant start_note21 : integer := end_note20 + 3;
-	constant end_note21 : integer := 27 + start_note21;
-	constant start_note22 : integer := end_note21 + 3;
-	constant end_note22 : integer := 27 + start_note22;
-	
-	constant start_black1 : integer := end_note1 - 5;
-	constant end_black1 : integer := start_note2 + 6;
-	constant start_black2 : integer := end_note2 - 5;
-	constant end_black2 : integer := start_note3 + 6;
-	constant start_black3 : integer := end_note4 - 5;
-	constant end_black3 : integer := start_note5 + 6;
-	constant start_black4 : integer := end_note5 - 5;
-	constant end_black4 : integer := start_note6 + 6;
-	constant start_black5 : integer := end_note6 - 5;
-	constant end_black5 : integer := start_note7 + 6;
-	constant start_black6 : integer := end_note8 - 5;
-	constant end_black6 : integer := start_note9 + 6;
-	constant start_black7 : integer := end_note9 - 5;
-	constant end_black7 : integer := start_note10 + 6;
-	constant start_black8 : integer := end_note11 - 5;
-	constant end_black8 : integer := start_note12 + 6;
-	constant start_black9 : integer := end_note12 - 5;
-	constant end_black9 : integer := start_note13 + 6;
-	constant start_black10 : integer := end_note13 - 5;
-	constant end_black10 : integer := start_note14 + 6;
-	constant start_black11 : integer := end_note15 - 5;
-	constant end_black11 : integer := start_note16 + 6;
-	constant start_black12 : integer := end_note16 - 5;
-	constant end_black12 : integer := start_note17 + 6;
-	constant start_black13 : integer := end_note18 - 5;
-	constant end_black13 : integer := start_note19 + 6;
-	constant start_black14 : integer := end_note19 - 5;
-	constant end_black14 : integer := start_note20 + 6;
-	constant start_black15 : integer := end_note20 - 5;
-	constant end_black15 : integer := start_note21 + 6;
+constant note_width 			: integer := 44;
+constant note_gap_width 	: integer := 2;
+constant black_pre_width 	: integer := 10;
+constant black_post_width 	: integer := 11;
+
+constant start_note1 : integer := 1;
+constant end_note1 : integer := start_note1 + note_width;
+constant start_note2 : integer := end_note1 + note_gap_width;
+constant end_note2 : integer := note_width + start_note2;
+
+constant start_note3 : integer := end_note2 + note_gap_width;
+constant end_note3 : integer := note_width + start_note3;
+constant start_note4 : integer := end_note3 + note_gap_width;
+constant end_note4 : integer := note_width + start_note4;
+constant start_note5 : integer := end_note4 + note_gap_width;
+constant end_note5 : integer := note_width + start_note5;
+constant start_note6 : integer := end_note5 + note_gap_width;
+constant end_note6 : integer := note_width + start_note6;
+constant start_note7 : integer := end_note6 + note_gap_width;
+constant end_note7 : integer := note_width + start_note7;
+constant start_note8 : integer := end_note7 + note_gap_width;
+constant end_note8 : integer := note_width + start_note8;
+constant start_note9 : integer := end_note8 + note_gap_width;
+constant end_note9 : integer := note_width + start_note9;
+constant start_note10 : integer := end_note9 + note_gap_width;
+constant end_note10 : integer := note_width + start_note10;
+constant start_note11 : integer := end_note10 + note_gap_width;
+constant end_note11 : integer := note_width + start_note11;
+constant start_note12 : integer := end_note11 + note_gap_width;
+constant end_note12 : integer := note_width + start_note12;
+constant start_note13 : integer := end_note12 + note_gap_width;
+constant end_note13 : integer := note_width + start_note13;
+constant start_note14 : integer := end_note13 + note_gap_width;
+constant end_note14 : integer := note_width + start_note14;
+
+constant start_black1 : integer := end_note1 - black_pre_width;
+constant end_black1 : integer := start_note2 + black_post_width;
+constant start_black2 : integer := end_note2 - black_pre_width;
+constant end_black2 : integer := start_note3 + black_post_width;
+constant start_black3 : integer := end_note4 - black_pre_width;
+constant end_black3 : integer := start_note5 + black_post_width;
+constant start_black4 : integer := end_note5 - black_pre_width;
+constant end_black4 : integer := start_note6 + black_post_width;
+constant start_black5 : integer := end_note6 - black_pre_width;
+constant end_black5 : integer := start_note7 + black_post_width;
+constant start_black6 : integer := end_note8 - black_pre_width;
+constant end_black6 : integer := start_note9 + black_post_width;
+constant start_black7 : integer := end_note9 - black_pre_width;
+constant end_black7 : integer := start_note10 + black_post_width;
+constant start_black8 : integer := end_note11 - black_pre_width;
+constant end_black8 : integer := start_note12 + black_post_width;
+constant start_black9 : integer := end_note12 - black_pre_width;
+constant end_black9 : integer := start_note13 + black_post_width;
+constant start_black10 : integer := end_note13 - black_pre_width;
+constant end_black10 : integer := start_note14 + black_post_width;
 	
 	constant top_y : integer := 381;
 	constant bot_y : integer := 459;
@@ -123,8 +103,8 @@ begin
 		elsif rising_edge(clk) then
 			drawing_request <= '0';
 			if keys(0) = '1' then
-				if ((oCoord_y >= top_y AND oCoord_y < bot_y) AND (oCoord_X >= start_note8 AND oCoord_X <= start_black6)) OR
-					((oCoord_y >= bot_y AND oCoord_y < y_frame) AND (oCoord_X >= start_note8 AND oCoord_X <= end_note8)) then
+				if ((oCoord_y >= top_y AND oCoord_y < bot_y) AND (oCoord_X >= start_note1 AND oCoord_X <= start_black1)) OR
+					((oCoord_y >= bot_y AND oCoord_y < y_frame) AND (oCoord_X >= start_note1 AND oCoord_X <= end_note1)) then
 					drawing_request <= '1';
 					mVGA_R <= "000";	
 					mVGA_G <= "111";	
@@ -132,8 +112,8 @@ begin
 				end if;
 			end if;
 			if keys(1) = '1' then
-				if ((oCoord_y >= top_y AND oCoord_y < bot_y) AND (oCoord_X >= end_black6 AND oCoord_X <= start_black7)) OR
-					((oCoord_y >= bot_y AND oCoord_y < y_frame) AND (oCoord_X >= start_note9 AND oCoord_X <= end_note9)) then
+				if ((oCoord_y >= top_y AND oCoord_y < bot_y) AND (oCoord_X >= end_black1 AND oCoord_X <= start_black2)) OR
+					((oCoord_y >= bot_y AND oCoord_y < y_frame) AND (oCoord_X >= start_note2 AND oCoord_X <= end_note2)) then
 					drawing_request <= '1';
 					mVGA_R <= "000";	
 					mVGA_G <= "111";	
@@ -141,8 +121,8 @@ begin
 				end if;
 			end if;
 			if keys(2) = '1' then
-				if ((oCoord_y >= top_y AND oCoord_y < bot_y) AND (oCoord_X >= end_black7 AND oCoord_X <= end_note10)) OR
-					((oCoord_y >= bot_y AND oCoord_y < y_frame) AND (oCoord_X >= start_note10 AND oCoord_X <= end_note10)) then
+				if ((oCoord_y >= top_y AND oCoord_y < bot_y) AND (oCoord_X >= end_black2 AND oCoord_X <= end_note3)) OR
+					((oCoord_y >= bot_y AND oCoord_y < y_frame) AND (oCoord_X >= start_note3 AND oCoord_X <= end_note3)) then
 					drawing_request <= '1';
 					mVGA_R <= "000";	
 					mVGA_G <= "111";	
@@ -150,8 +130,8 @@ begin
 				end if;
 			end if;
 			if keys(3) = '1' then
-				if ((oCoord_y >= top_y AND oCoord_y < bot_y) AND (oCoord_X >= start_note11 AND oCoord_X <= start_black8)) OR
-					((oCoord_y >= bot_y AND oCoord_y < y_frame) AND (oCoord_X >= start_note11 AND oCoord_X <= end_note11)) then
+				if ((oCoord_y >= top_y AND oCoord_y < bot_y) AND (oCoord_X >= start_note4 AND oCoord_X <= start_black3)) OR
+					((oCoord_y >= bot_y AND oCoord_y < y_frame) AND (oCoord_X >= start_note4 AND oCoord_X <= end_note4)) then
 					drawing_request <= '1';
 					mVGA_R <= "000";	
 					mVGA_G <= "111";	
@@ -159,8 +139,8 @@ begin
 				end if;
 			end if;
 			if keys(4) = '1' then
-				if ((oCoord_y >= top_y AND oCoord_y < bot_y) AND (oCoord_X >= end_black8 AND oCoord_X <= start_black9)) OR
-					((oCoord_y >= bot_y AND oCoord_y < y_frame) AND (oCoord_X >= start_note12 AND oCoord_X <= end_note12)) then
+				if ((oCoord_y >= top_y AND oCoord_y < bot_y) AND (oCoord_X >= end_black3 AND oCoord_X <= start_black4)) OR
+					((oCoord_y >= bot_y AND oCoord_y < y_frame) AND (oCoord_X >= start_note5 AND oCoord_X <= end_note5)) then
 					drawing_request <= '1';
 					mVGA_R <= "000";	
 					mVGA_G <= "111";	
@@ -168,8 +148,8 @@ begin
 				end if;
 			end if;
 			if keys(5) = '1' then
-				if ((oCoord_y >= top_y AND oCoord_y < bot_y) AND (oCoord_X >= end_black9 AND oCoord_X <= start_black10)) OR
-					((oCoord_y >= bot_y AND oCoord_y < y_frame) AND (oCoord_X >= start_note13 AND oCoord_X <= end_note13)) then
+				if ((oCoord_y >= top_y AND oCoord_y < bot_y) AND (oCoord_X >= end_black4 AND oCoord_X <= start_black5)) OR
+					((oCoord_y >= bot_y AND oCoord_y < y_frame) AND (oCoord_X >= start_note6 AND oCoord_X <= end_note6)) then
 					drawing_request <= '1';
 					mVGA_R <= "000";	
 					mVGA_G <= "111";	
@@ -177,8 +157,8 @@ begin
 				end if;
 			end if;
 			if keys(6) = '1' then
-				if ((oCoord_y >= top_y AND oCoord_y < bot_y) AND (oCoord_X >= end_black10 AND oCoord_X <= end_note14)) OR
-					((oCoord_y >= bot_y AND oCoord_y < y_frame) AND (oCoord_X >= start_note14 AND oCoord_X <= end_note14)) then
+				if ((oCoord_y >= top_y AND oCoord_y < bot_y) AND (oCoord_X >= end_black5 AND oCoord_X <= end_note7)) OR
+					((oCoord_y >= bot_y AND oCoord_y < y_frame) AND (oCoord_X >= start_note7 AND oCoord_X <= end_note7)) then
 					drawing_request <= '1';
 					mVGA_R <= "000";	
 					mVGA_G <= "111";	
--- project/quartus/note_logic/note_draw_manager.vhd
+++ project/quartus/note_logic/note_draw_manager.vhd
@@ -42,81 +42,61 @@ architecture note_draw_manager_arch of note_draw_manager is
 	);
 	end component;
 	
-	constant start_note1 : integer := 6;
-	constant end_note1 : integer := 27;
-	constant start_note2 : integer := end_note1 + 3;
-	constant end_note2 : integer := 27 + start_note2;
-	constant start_note3 : integer := end_note2 + 3;
-	constant end_note3 : integer := 27 + start_note3;
-	constant start_note4 : integer := end_note3 + 3;
-	constant end_note4 : integer := 27 + start_note4;
-	constant start_note5 : integer := end_note4 + 3;
-	constant end_note5 : integer := 27 + start_note5;
-	constant start_note6 : integer := end_note5 + 3;
-	constant end_note6 : integer := 27 + start_note6;
-	constant start_note7 : integer := end_note6 + 3;
-	constant end_note7 : integer := 27 + start_note7;
-	constant start_note8 : integer := end_note7 + 3;
-	constant end_note8 : integer := 27 + start_note8;
-	constant start_note9 : integer := end_note8 + 3;
-	constant end_note9 : integer := 27 + start_note9;
-	constant start_note10 : integer := end_note9 + 3;
-	constant end_note10 : integer := 27 + start_note10;
-	constant start_note11 : integer := end_note10 + 3;
-	constant end_note11 : integer := 27 + start_note11;
-	constant start_note12 : integer := end_note11 + 3;
-	constant end_note12 : integer := 27 + start_note12;
-	constant start_note13 : integer := end_note12 + 3;
-	constant end_note13 : integer := 27 + start_note13;
-	constant start_note14 : integer := end_note13 + 3;
-	constant end_note14 : integer := 27 + start_note14;
-	constant start_note15 : integer := end_note14 + 3;
-	constant end_note15 : integer := 27 + start_note15;
-	constant start_note16 : integer := end_note15 + 3;
-	constant end_note16 : integer := 27 + start_note16;
-	constant start_note17 : integer := end_note16 + 3;
-	constant end_note17 : integer := 27 + start_note17;
-	constant start_note18 : integer := end_note17 + 3;
-	constant end_note18 : integer := 27 + start_note18;
-	constant start_note19 : integer := end_note18 + 3;
-	constant end_note19 : integer := 27 + start_note19;
-	constant start_note20 : integer := end_note19 + 3;
-	constant end_note20 : integer := 27 + start_note20;
-	constant start_note21 : integer := end_note20 + 3;
-	constant end_note21 : integer := 27 + start_note21;
-	constant start_note22 : integer := end_note21 + 3;
-	constant end_note22 : integer := 27 + start_note22;
-	
-	constant start_black1 : integer := end_note1 - 5;
-	constant end_black1 : integer := start_note2 + 6;
-	constant start_black2 : integer := end_note2 - 5;
-	constant end_black2 : integer := start_note3 + 6;
-	constant start_black3 : integer := end_note4 - 5;
-	constant end_black3 : integer := start_note5 + 6;
-	constant start_black4 : integer := end_note5 - 5;
-	constant end_black4 : integer := start_note6 + 6;
-	constant start_black5 : integer := end_note6 - 5;
-	constant end_black5 : integer := start_note7 + 6;
-	constant start_black6 : integer := end_note8 - 5;
-	constant end_black6 : integer := start_note9 + 6;
-	constant start_black7 : integer := end_note9 - 5;
-	constant end_black7 : integer := start_note10 + 6;
-	constant start_black8 : integer := end_note11 - 5;
-	constant end_black8 : integer := start_note12 + 6;
-	constant start_black9 : integer := end_note12 - 5;
-	constant end_black9 : integer := start_note13 + 6;
-	constant start_black10 : integer := end_note13 - 5;
-	constant end_black10 : integer := start_note14 + 6;
-	constant start_black11 : integer := end_note15 - 5;
-	constant end_black11 : integer := start_note16 + 6;
-	constant start_black12 : integer := end_note16 - 5;
-	constant end_black12 : integer := start_note17 + 6;
-	constant start_black13 : integer := end_note18 - 5;
-	constant end_black13 : integer := start_note19 + 6;
-	constant start_black14 : integer := end_note19 - 5;
-	constant end_black14 : integer := start_note20 + 6;
-	constant start_black15 : integer := end_note20 - 5;
-	constant end_black15 : integer := start_note21 + 6;
+constant note_width 			: integer := 44;
+constant note_gap_width 	: integer := 2;
+constant black_pre_width 	: integer := 10;
+constant black_post_width 	: integer := 11;
+
+constant start_note1 : integer := 1;
+constant end_note1 : integer := start_note1 + note_width;
+constant start_note2 : integer := end_note1 + note_gap_width;
+constant end_note2 : integer := note_width + start_note2;
+
+constant start_note3 : integer := end_note2 + note_gap_width;
+constant end_note3 : integer := note_width + start_note3;
+constant start_note4 : integer := end_note3 + note_gap_width;
+constant end_note4 : integer := note_width + start_note4;
+constant start_note5 : integer := end_note4 + note_gap_width;
+constant end_note5 : integer := note_width + start_note5;
+constant start_note6 : integer := end_note5 + note_gap_width;
+constant end_note6 : integer := note_width + start_note6;
+constant start_note7 : integer := end_note6 + note_gap_width;
+constant end_note7 : integer := note_width + start_note7;
+constant start_note8 : integer := end_note7 + note_gap_width;
+constant end_note8 : integer := note_width + start_note8;
+constant start_note9 : integer := end_note8 + note_gap_width;
+constant end_note9 : integer := note_width + start_note9;
+constant start_note10 : integer := end_note9 + note_gap_width;
+constant end_note10 : integer := note_width + start_note10;
+constant start_note11 : integer := end_note10 + note_gap_width;
+constant end_note11 : integer := note_width + start_note11;
+constant start_note12 : integer := end_note11 + note_gap_width;
+constant end_note12 : integer := note_width + start_note12;
+constant start_note13 : integer := end_note12 + note_gap_width;
+constant end_note13 : integer := note_width + start_note13;
+constant start_note14 : integer := end_note13 + note_gap_width;
+constant end_note14 : integer := note_width + start_note14;
+
+constant start_black1 : integer := end_note1 - black_pre_width;
+constant end_black1 : integer := start_note2 + black_post_width;
+constant start_black2 : integer := end_note2 - black_pre_width;
+constant end_black2 : integer := start_note3 + black_post_width;
+constant start_black3 : integer := end_note4 - black_pre_width;
+constant end_black3 : integer := start_note5 + black_post_width;
+constant start_black4 : integer := end_note5 - black_pre_width;
+constant end_black4 : integer := start_note6 + black_post_width;
+constant start_black5 : integer := end_note6 - black_pre_width;
+constant end_black5 : integer := start_note7 + black_post_width;
+constant start_black6 : integer := end_note8 - black_pre_width;
+constant end_black6 : integer := start_note9 + black_post_width;
+constant start_black7 : integer := end_note9 - black_pre_width;
+constant end_black7 : integer := start_note10 + black_post_width;
+constant start_black8 : integer := end_note11 - black_pre_width;
+constant end_black8 : integer := start_note12 + black_post_width;
+constant start_black9 : integer := end_note12 - black_pre_width;
+constant end_black9 : integer := start_note13 + black_post_width;
+constant start_black10 : integer := end_note13 - black_pre_width;
+constant end_black10 : integer := start_note14 + black_post_width;
 	
 	signal notes_request : std_logic_vector(0 to 6);
 	
@@ -126,26 +106,26 @@ begin
 	
 	drawing_request <= '1' when notes_request > 0 else '0';
 	
-	note1: note_draw port map (CLK, RESETn, std_logic_vector(to_unsigned(start_note8, 9)), 
-				std_logic_vector(to_unsigned(end_note8, 9)), 
+	note1: note_draw port map (CLK, RESETn, std_logic_vector(to_unsigned(start_note1, 9)), 
+				std_logic_vector(to_unsigned(end_note1, 9)), 
 				oCoord_X, oCoord_Y, timer_done, notes(0), notes_enable, notes_request(0));
-	note2: note_draw port map (CLK, RESETn, std_logic_vector(to_unsigned(start_note9, 9)), 
-				std_logic_vector(to_unsigned(end_note9, 9)), 
+	note2: note_draw port map (CLK, RESETn, std_logic_vector(to_unsigned(start_note2, 9)), 
+				std_logic_vector(to_unsigned(end_note2, 9)), 
 				oCoord_X, oCoord_Y, timer_done, notes(1), notes_enable, notes_request(1));
-	note3: note_draw port map (CLK, RESETn, std_logic_vector(to_unsigned(start_note10, 9)), 
-				std_logic_vector(to_unsigned(end_note10, 9)), 
+	note3: note_draw port map (CLK, RESETn, std_logic_vector(to_unsigned(start_note3, 9)), 
+				std_logic_vector(to_unsigned(end_note3, 9)), 
 				oCoord_X, oCoord_Y, timer_done, notes(2), notes_enable, notes_request(2));
-	note4: note_draw port map (CLK, RESETn, std_logic_vector(to_unsigned(start_note11, 9)), 
-				std_logic_vector(to_unsigned(end_note11, 9)), 
+	note4: note_draw port map (CLK, RESETn, std_logic_vector(to_unsigned(start_note4, 9)), 
+				std_logic_vector(to_unsigned(end_note4, 9)), 
 				oCoord_X, oCoord_Y, timer_done, notes(3), notes_enable, notes_request(3));
-	note5: note_draw port map (CLK, RESETn, std_logic_vector(to_unsigned(start_note12, 9)), 
-				std_logic_vector(to_unsigned(end_note12, 9)), 
+	note5: note_draw port map (CLK, RESETn, std_logic_vector(to_unsigned(start_note5, 9)), 
+				std_logic_vector(to_unsigned(end_note5, 9)), 
 				oCoord_X, oCoord_Y, timer_done, notes(4), notes_enable, notes_request(4));
-	note6: note_draw port map (CLK, RESETn, std_logic_vector(to_unsigned(start_note13, 9)), 
-				std_logic_vector(to_unsigned(end_note13, 9)), 
+	note6: note_draw port map (CLK, RESETn, std_logic_vector(to_unsigned(start_note6, 9)), 
+				std_logic_vector(to_unsigned(end_note6, 9)), 
 				oCoord_X, oCoord_Y, timer_done, notes(5), notes_enable, notes_request(5));
-	note7: note_draw port map (CLK, RESETn, std_logic_vector(to_unsigned(start_note14, 9)), 
-				std_logic_vector(to_unsigned(end_note14, 9)), 
+	note7: note_draw port map (CLK, RESETn, std_logic_vector(to_unsigned(start_note7, 9)), 
+				std_logic_vector(to_unsigned(end_note7, 9)), 
 				oCoord_X, oCoord_Y, timer_done, notes(6), notes_enable, notes_request(6));
 
 end note_draw_manager_arch;	
